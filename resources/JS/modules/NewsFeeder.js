fbe9562dd4537070fc734b6f5b9e489e0e32a568
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-24 06:29:55 +0530

    Update NewsFeeder.js

--- resources/JS/modules/NewsFeeder.js
+++ resources/JS/modules/NewsFeeder.js
@@ -14,7 +14,7 @@ export default class NewsFeeder extends CommonFunctions {
   	};
 
   	return this.serverCall("expenses/api/createGroup",extraParameters); */
-		this.rssFeeder("GET https://newsapi.org/v2/everything?q=keyword&apiKey=d54f337feac8491db583ec06fd854eef");
+		this.rssFeeder("https://newsapi.org/v2/top-headlines?country=in&apiKey=d54f337feac8491db583ec06fd854eef");
 	console.log("yes");
 	return false;
 		
